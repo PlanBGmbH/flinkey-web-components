00328c9b6dd2d32a5260467facac41013c4bdda0
Bernhard Huber <bernhard.huber@plan-b-gmbh.com>
2021-05-17 23:23:44 +0200

    👷 changed PR pipe

---
+++ sdf.sh
@@ -0,0 +1,18 @@
+gitDiff=$( git diff --exit-code origin/main -- components )
+echo $gitDiff
+if [ $gitDiff = 1 ];
+then
+    echo 'components directory content changed'
+    pullRequestPackageVersion=$( jq -r '.version' package.json )
+    echo "PR package version: $pullRequestPackageVersion"
+    echo "Latest package version on main: ${{ env.mainPackageVersion }}"
+    if $(dpkg --compare-versions $pullRequestPackageVersion gt ${{ env.mainPackageVersion }});
+    then
+        echo 'Version updated'
+    else
+        echo 'Version did not update'
+        exit 1
+    fi
+else
+    echo 'components directory content did not change'
+fi
