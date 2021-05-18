5d538cddcdb24693682c95648c70a849957d5af2
Bernhard Huber <bernhard.huber@plan-b-gmbh.com>
2021-05-18 21:57:44 +0200

    💚 fixed PR pipe script

--- scripts/workflows/check_package_version_update.sh
+++ scripts/workflows/check_package_version_update.sh
@@ -10,17 +10,17 @@ echo "$( jq '.version = "null"' ./main/components/package-lock.json )" > ./main/
 echo "Calculate diff between main and PR branch"
 diff -qr pr/components main/components && changes=0 || changes=1
 echo "Re-assign version of package.json and package-lock.json"
-echo "$( jq '.version = "${{ env.prPackageVersion }}"' ./pr/components/package.json )" > ./pr/components/package.json
-echo "$( jq '.version = "${{ env.prPackageLockVersion }}"' ./pr/components/package-lock.json )" > ./pr/components/package-lock.json
+echo "$( jq '.version = "$prPackageVersion"' ./pr/components/package.json )" > ./pr/components/package.json
+echo "$( jq '.version = "$prPackageLockVersion"' ./pr/components/package-lock.json )" > ./pr/components/package-lock.json
 echo "Update prPackageVersion environment variable"
-prPackageVersion=$( jq -r '.version' ./pr/components/package.json )
-echo "prPackageVersion=$prPackageVersion" >> $GITHUB_ENV
+currentPrPackageVersion=$( jq -r '.version' ./pr/components/package.json )
+echo "prPackageVersion=$currentPrPackageVersion" >> $GITHUB_ENV
 if [ $changes -eq 1 ];
 then
   echo 'components directory content changed'
-  echo "PR package version: ${{ env.prPackageVersion }}"
+  echo "PR package version: $currentPrPackageVersion"
   echo "Main package version: $mainPackageVersion"
-  if $(dpkg --compare-versions ${{ env.prPackageVersion }} gt $mainPackageVersion);
+  if $(dpkg --compare-versions $currentPrPackageVersion gt $mainPackageVersion);
   then
     echo 'Version updated'
   else
@@ -29,7 +29,7 @@ then
   fi
 else
   echo 'components directory content did not change'
-  if $(dpkg --compare-versions ${{ env.prPackageVersion }} ne $mainPackageVersion);
+  if $(dpkg --compare-versions $currentPrPackageVersion ne $mainPackageVersion);
   then
     echo 'Version changed but should not'
     exit 1
