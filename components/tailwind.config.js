f43f7915c92cc553e3ba20d86fdaab4af9257b43
roswerk <73542409+roswerk@users.noreply.github.com>
2022-01-27 14:02:39 +0100

    Created tailwind.config file

---
+++ components/tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.css'],
+  content: [],
+  theme: {
+    extend: {},
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900,
+    },
+  },
+  plugins: [],
+};
