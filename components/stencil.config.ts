a26d161a0a77afb6724e45844f21a9906e72e156
roswerk <73542409+roswerk@users.noreply.github.com>
2022-01-31 11:52:14 +0100

    🔧 Updated Tailwind integration with project

--- components/stencil.config.ts
+++ components/stencil.config.ts
@@ -1,9 +1,17 @@
 import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
 
 import { postcss } from '@stencil/postcss';
-import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import cssnano from 'cssnano';
+import purgecss from '@fullhuman/postcss-purgecss';
+
+//purge function to keep only the classes used in EACH component
+const purge = purgecss({
+  content: ['./src/**/*.tsx', './src/index.html'],
+  safelist: [':host'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 export const config: Config = {
   namespace: 'flinkey-web-components',
@@ -25,7 +33,11 @@ export const config: Config = {
   ],
   plugins: [
     postcss({
-      plugins: [tailwindcss(), autoprefixer],
+      // add postcss plugins
+      plugins: [tailwindcss('./tailwind.config.js'), autoprefixer(), ...(process.env.NODE_ENV === 'production' ? [purgecss, require('cssnano')] : [])],
     }),
   ],
+  devServer: {
+    reloadStrategy: 'pageReload',
+  },
 };
