64a4cab7f403d118845711c8a4c3f25f9412b73a
roswerk <73542409+roswerk@users.noreply.github.com>
2022-02-09 14:12:04 +0100

    🚧 Added service-dropdown to modal

--- components/src/components/flinkey-service-assigner/flinkey-service-modal/flinkey-modal.tsx
+++ components/src/components/flinkey-service-assigner/flinkey-service-modal/flinkey-modal.tsx
@@ -60,9 +60,13 @@ export class FlinkeyModal {
                 <button class="mr-3 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-slate-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto sm:text-sm">
                   {this.product}
                 </button>
+                {this.service === 0 ? (
+                  <flinkey-services-dropdown unlinkedServices={this.unlinkedServices} />
+                ) : (
                   <button class="mr-3 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-slate-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto sm:text-sm">
                     {this.service}
                   </button>
+                )}
               </div>
               <div>
                 {this.service === 0 ? (
