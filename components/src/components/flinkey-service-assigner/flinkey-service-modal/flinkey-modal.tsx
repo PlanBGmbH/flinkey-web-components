3ec2f321832aec74ec836c4a51925602be7aeea7
roswerk <73542409+roswerk@users.noreply.github.com>
2022-02-09 13:29:55 +0100

    🍱 Added Flinkey Modal file

---
+++ components/src/components/flinkey-service-assigner/flinkey-service-modal/flinkey-modal.tsx
@@ -0,0 +1,12 @@
+import { Component, Event, EventEmitter, h, Prop, Listen, State } from '@stencil/core';
+@Component({
+  tag: 'flinkey-modal',
+  styleUrl: '../../../utils/common.css',
+  shadow: true,
+})
+export class FlinkeyModal {
+  render() {
+    return (
+    );
+  }
+}
