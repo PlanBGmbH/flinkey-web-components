4fe11a96d548860184fee9e63ecdd3c94f6b5814
roswerk <73542409+roswerk@users.noreply.github.com>
2022-02-09 12:47:09 +0100

    🍱Added flinkey-product-service-admin-table component file

---
+++ components/src/components/flinkey-service-assigner/flinkey-service-assigner.tsx
@@ -0,0 +1,14 @@
+import { Component, h, State, Watch, Listen, Host } from '@stencil/core';
+import { httpGet, HttpResponse } from '../../utils/utils';
+
+@Component({
+  tag: 'flinkey-product-service-admin-table',
+  styleUrl: '../../utils/common.css',
+  shadow: true,
+})
+export class ProductServiceAdminTable {
+  render() {
+    return (
+    );
+  }
+}
