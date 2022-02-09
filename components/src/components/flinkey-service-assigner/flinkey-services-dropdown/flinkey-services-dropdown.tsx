61d4679789b038ae7865956e5caf2e180a57cd1e
roswerk <73542409+roswerk@users.noreply.github.com>
2022-02-09 13:41:07 +0100

    🍱 Added service-dropdown file

---
+++ components/src/components/flinkey-service-assigner/flinkey-services-dropdown/flinkey-services-dropdown.tsx
@@ -0,0 +1,13 @@
+import { Component, h, State, Prop, EventEmitter, Event } from '@stencil/core';
+
+@Component({
+  tag: 'flinkey-services-dropdown',
+  styleUrl: '../../../utils/common.css',
+  shadow: true,
+})
+export class ServiceDropdown {
+  render() {
+    return (
+    );
+  }
+}
